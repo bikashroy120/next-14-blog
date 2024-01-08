cf356e0a9d5f524421413d4a20a420262393c109
bikashroy120 <bikashroydt@gmail.com>
2024-01-08 21:09:20 +0600

    about and contact us pag add

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+// "use client";
+import Image from "next/image";
+import styles from "./contact.module.css";
+import Container from "@/ui/Container";
+// import dynamic from "next/dynamic";
+// import HydrationTest from "@/components/hydrationTest";
 
-const Page = () => {
+// const HydrationTestNoSSR = dynamic(()=>import("@/components/hydrationTest"), {ssr: false})
+
+export const metadata = {
+  title: "Contact Page",
+  description: "Contact description",
+};
+
+const ContactPage = () => {
+  // const a = Math.random();
+
+  // console.log(a);
+
   return (
-    <div>Contact Page</div>
-  )
-}
+    <div className=" w-full">
+      <Container>
+        <div className=" mt-[100px] mb-[80px]">
+          <div>
+            <div className={styles.container}>
+              <div className={styles.imgContainer}>
+                <Image src="/contact.png" alt="" fill className={styles.img} />
+              </div>
+              <div className={styles.formContainer}>
+                {/* <HydrationTestNoSSR/> */}
+                {/* <div suppressHydrationWarning>{a}</div> */}
+                <form action="" className={styles.form}>
+                  <input type="text" placeholder="Name and Surname" />
+                  <input type="text" placeholder="Email Address" />
+                  <input type="text" placeholder="Phone Number (Optional)" />
+                  <textarea
+                    name=""
+                    id=""
+                    cols="30"
+                    rows="10"
+                    placeholder="Message"
+                  ></textarea>
+                  <button className=" py-2 px-5 bg-green-500 rounded-lg text-white">Send</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
 
-export default Page
+export default ContactPage;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,18 @@
+import Container from '@/ui/Container'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <Container>
+        <div className=' flex items-center justify-between'>
+            <div>
+                Bikash Roy
+            </div>
+            <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</div>
+        </div>
+      </Container>
+    </div>
   )
 }
 
